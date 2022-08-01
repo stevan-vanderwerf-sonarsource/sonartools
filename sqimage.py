beebad153c03f3cd161b1b0081f9329896f724e4
Stevan <stevan.vanderwerf@sonarsource.com>
2022-07-31 21:12:52 -0500

    added env vars proof of concept

--- sqimage.py
+++ sqimage.py
@@ -1,5 +1,12 @@
 import subprocess
+import os
 
-subprocess.call([
-    'docker-compose', '--file', './docker-compose1.yml', 'up', '-d',
-    ])
+myenv = {
+    **os.environ,
+    "BUILD": str('899'),
+    "SQ_VERSION": str('8.9.9-enterprise')
+}
+
+process = subprocess.Popen(['docker-compose', '--file', "/home/stevanvanderwerf/code/tools/docker-compose.yml.bak2", '--verbose', 'up', '-d'], env=myenv)
+process.wait()
+print("Completed!")
