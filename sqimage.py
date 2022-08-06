d88d7d87c6a6a3c0f3e19b0fe5f39052e5795ddf
Stevan <stevan.vanderwerf@sonarsource.com>
2022-08-05 20:12:51 -0500

    added parameters with tag options

--- sqimage.py
+++ sqimage.py
@@ -1,10 +1,24 @@
 import subprocess
 import os
+import sys
+
+# from https://github.com/axil/docker-registry-list
+tags = {
+    "ee950":"9.5.0-enterprise",
+    "ee890":"8.9.0-enterprise"
+}
+
+# e.g. ee950
+version = sys.argv[1]
+# e.g. 9.5.0-enterprise
+tag = tags.get(version)
+# e.g. 950
+tag_num = version[len(version)-3:]
 
 myenv = {
     **os.environ,
-    "BUILD": str('899'),
-    "SQ_VERSION": str('8.9.9-enterprise')
+    "BUILD": str(tag_num),
+    "SQ_VERSION": str(tag)
 }
 
 process = subprocess.Popen(['docker-compose', '--file', "/home/stevanvanderwerf/code/tools/docker-compose.yml.bak2", '--verbose', 'up', '-d'], env=myenv)
