66e77eabd5862d3d2679f212c2670aee2f1102c9
Stevan <stevan.vanderwerf@sonarsource.com>
2022-08-07 15:00:27 -0500

    added profile option for db types

--- sqimage.py
+++ sqimage.py
@@ -15,23 +15,26 @@ tag = tags.get(version)
 # e.g. 950
 tag_num = version[len(version)-3:]
 
+# set database type, defaults to h2 if not set
+db_type = 'h2' if len(sys.argv) < 3 else sys.argv[2]
+
 myenv = {
     **os.environ,
     "BUILD": str(tag_num),
-    "SQ_VERSION": str(tag)
+    "SQ_VERSION": str(tag),
+    "DB": str(db_type)
 }
 
 # enables the ability to spin up another instance of a different version to compare
-compare = True
+compare = False
 docker_compose_file = 'docker-compose_script_compare.yml' if compare else 'docker-compose_script.yml'
 
-process = subprocess.Popen([ \
-    'docker-compose', \
-    '--file', "/home/stevanvanderwerf/code/tools/" + docker_compose_file, \
-    '--verbose', \
-    'up', \
-    '-d'], \
-    env=myenv)
+# enables ability to add parameters for db options, version 1.28.0 or later of docker-compose is required
+db = True
+profile = "--profile dbs up" if db else "up"
+
+process_cmd = f"docker-compose --file /home/stevanvanderwerf/code/tools/{docker_compose_file} --verbose {profile} -d"
+process = subprocess.Popen(process_cmd, shell=True, env=myenv)
 
 process.wait()
 print("Completed!")
