3bd16863b14fa2b52a9ea0376d88687d8b6124de
Stevan <stevan.vanderwerf@sonarsource.com>
2022-08-10 19:56:36 -0500

    initial commit

--- sq.py
+++ sq.py
@@ -1,6 +1,4 @@
-# Import the argparse library
 import argparse
-
 import os
 import sys
 
@@ -9,13 +7,30 @@ my_parser = argparse.ArgumentParser(
     prog='sq', 
     description='run any version or edition of SQ in a docker container')
 
-# Add the arguments
+# positional argument
 my_parser.add_argument(
     'sqEditionVersion',
     metavar='sqEdVer',
     type=str,
     help='SQ edition and version e.g. ee899')
 
+# optional arguments
+my_parser.add_argument(
+    '-vv',
+    '--verbose',
+    action='store_true',
+    help='enable verbose output')
+
+my_parser.add_argument(
+    '-db',
+    '--database',
+    action='store',
+    nargs=1,
+    choices=['pg', 'ms', 'orc'],
+    help='choose external database')
+
 args = my_parser.parse_args()
 print(args)
 print(args.sqEditionVersion)
+print(args.database)
+
