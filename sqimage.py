68b565761eb81e7ff19ef0031fb56c0378982fa7
Stevan <stevan.vanderwerf@sonarsource.com>
2022-08-10 19:55:47 -0500

    moved env into this script

--- sqimage.py
+++ sqimage.py
@@ -5,7 +5,7 @@ import sys
 # from https://github.com/axil/docker-registry-list
 tags = {
     "ee950":"9.5.0-enterprise",
-    "ee890":"8.9.0-enterprise"
+    "ee899":"8.9.9-enterprise"
 }
 
 # e.g. ee950
@@ -17,23 +17,36 @@ tag_num = version[len(version)-3:]
 
 # set database type, defaults to h2 if not set
 db_type = 'h2' if len(sys.argv) < 3 else sys.argv[2]
+print(db_type)
 
-myenv = {
+myenvh2 = {
     **os.environ,
     "BUILD": str(tag_num),
     "SQ_VERSION": str(tag),
     "DB": str(db_type)
 }
 
+myenvdb = {
+    **os.environ,
+    "BUILD": str(tag_num),
+    "SQ_VERSION": str(tag),
+    "DB": str(db_type),
+    "SONAR_JDBC_USERNAME": str('sonar'),
+    "SONAR_JDBC_PASSWORD": str('sonar'),
+    "SONAR_JDBC_URL": str('jdbc:postgresql://db:5432/sonardb')
+}
+# picks the right env variables based on database
+myenv = myenvh2 if db_type == 'h2' else myenvdb
+
 # enables the ability to spin up another instance of a different version to compare
 compare = False
 docker_compose_file = 'docker-compose_script_compare.yml' if compare else 'docker-compose_script.yml'
 
-# enables ability to add parameters for db options, version 1.28.0 or later of docker-compose is required
-db = True
+# enables ability to add parameters for db options, version 1.28.0 or later of docker-compose is required for --profile
+db = False
 profile = "--profile dbs up" if db else "up"
 
-process_cmd = f"docker-compose --file /home/stevanvanderwerf/code/tools/{docker_compose_file} --verbose {profile} -d"
+process_cmd = f"docker-compose --file ./{docker_compose_file} --verbose {profile} -d"
 process = subprocess.Popen(process_cmd, shell=True, env=myenv)
 
 process.wait()
