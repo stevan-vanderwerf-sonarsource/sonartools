fa235ba84dd88e7749a6807d1f118ab0197ac9ff
Stevan <stevan.vanderwerf@sonarsource.com>
2022-08-12 20:23:39 -0500

    added main file

---
+++ main.py
@@ -0,0 +1,58 @@
+from sq import parse_cli_args
+import subprocess
+import os
+import sys
+
+def main():
+    """Parses user input and passes those values to the Container object"""
+
+    # from https://github.com/axil/docker-registry-list
+    tags = {
+        "ee950":"9.5.0-enterprise",
+        "ee899":"8.9.9-enterprise"
+    }
+
+    output = parse_cli_args()
+    # e.g. ee950
+    version = output.up
+    # e.g. 9.5.0-enterprise
+    tag = tags.get(version)
+    # e.g. 950
+    tag_num = version[len(version)-3:]
+
+    # set database type, defaults to h2 if not set
+    db_type = 'h2' if output.database is None else output.database
+
+    myenvh2 = {
+        **os.environ,
+        "BUILD": str(tag_num),
+        "SQ_VERSION": str(tag),
+        "DB": str(db_type)
+    }
+
+    myenvdb = {
+        **os.environ,
+        "BUILD": str(tag_num),
+        "SQ_VERSION": str(tag),
+        "DB": str(db_type),
+        "SONAR_JDBC_USERNAME": str('sonar'),
+        "SONAR_JDBC_PASSWORD": str('sonar'),
+        "SONAR_JDBC_URL": str('jdbc:postgresql://db:5432/sonardb')
+    }
+    # picks the right env variables based on database
+    myenv = myenvh2 if db_type == 'h2' else myenvdb
+
+    # enables the ability to spin up another instance of a different version to compare
+    compare = False
+    docker_compose_file = 'docker-compose_script_compare.yml' if compare else 'docker-compose_script.yml'
+
+    # enables ability to add parameters for db options, version 1.28.0 or later of docker-compose is required for --profile
+    profile = "up" if db_type == 'h2' else "--profile dbs up"
+
+    process_cmd = f"docker-compose --file ./{docker_compose_file} --verbose {profile} -d"
+    process = subprocess.Popen(process_cmd, shell=True, env=myenv)
+
+    process.wait()
+
+if __name__ == "__main__":
+    main()
--- sq.py
+++ sq.py
@@ -2,35 +2,38 @@ import argparse
 import os
 import sys
 
-# Create the parser
-my_parser = argparse.ArgumentParser(
-    prog='sq', 
-    description='run any version or edition of SQ in a docker container')
+def parse_cli_args():
+    
+    my_parser = argparse.ArgumentParser(
+        prog='sq', 
+        description='run any version or edition of SQ in a docker container')
 
-# positional argument
-my_parser.add_argument(
-    'sqEditionVersion',
-    metavar='sqEdVer',
-    type=str,
-    help='SQ edition and version e.g. ee899')
+    # positional argument
+    my_parser.add_argument(
+        'up',
+        metavar='up',
+        type=str,
+        help='SQ edition and version e.g. ee899')
 
-# optional arguments
-my_parser.add_argument(
-    '-vv',
-    '--verbose',
-    action='store_true',
-    help='enable verbose output')
+    # optional arguments
+    my_parser.add_argument(
+        '-vv',
+        '--verbose',
+        action='store_true',
+        help='enable verbose output')
 
-my_parser.add_argument(
-    '-db',
-    '--database',
-    action='store',
-    nargs=1,
-    choices=['pg', 'ms', 'orc'],
-    help='choose external database')
+    my_parser.add_argument(
+        '-db',
+        '--database',
+        action='store',
+        nargs=1,
+        choices=['pg', 'ms', 'orc'],
+        help='choose external database')
 
-args = my_parser.parse_args()
-print(args)
-print(args.sqEditionVersion)
-print(args.database)
+    args = my_parser.parse_args()
+    return args
+
+# print(args)
+# print(args.up)
+# print(args.database)
 
